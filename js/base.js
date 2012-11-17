5b494b03cbb4620b6ccd419394b5b796ef6e7368
David Johnston <david712@iastate.edu>
2012-11-17 14:53:07 -0600

    Added action icons.

--- js/base.js
+++ js/base.js
@@ -1,17 +1,20 @@
-function setIconImages() {
-	$('div#file-system .folder img').attr({ src:"img/folder.png", alt:"folder", width:"48", height:"48" });
-	$('div#file-system .file img').attr({ src:"img/file.png", alt:"file", width:"48", height:"48" });
-}
 
+
+function pathBarUpdate(cwd)
+{
+	// TODO: interpret and add <div> elements according to cwd
+
+	//$('div#path-bar').add('<div>/home/username</div>');  // TODO: why doesn't adding an html fragment work?
+};
 
 function fileSystemUpdate($data) {
 	$('div#file-system').html($data);
-	setIconImages();
-}
+};
 
 
 // BUG: every time a submenu is clicked, another click is bound to each .menu-item
 // BUG: after a page load, the first attempt to click a .menu-item within a popupMenu seems to have no effect.
+// BUG: overlay does not cover entire document, only that part of the document which is initially visible.
 
 function popupOpen($menu, x, y) {
 
@@ -40,18 +43,23 @@ function popupClose($menu)
 
 $(document).ready(function() {
 
-	// fill div#file-system with some initial data
-	var $fileSystemData =
-	$("<div class='icon folder'> \
-		<img/> \
-		<span>Folder Name</span> \
-	</div> \
-	<div class='icon file'> \
-		<img/> \
-		<span>File Name</span> \
-	</div>");
+	// fill div#file-system and div#path-bar with some initial data:
+	var $fileSystemData = $(" \
+		<div class='icon folder'> \
+			<img/> \
+			<span>Folder Name</span> \
+		</div> \
+		<div class='icon file'> \
+			<img/> \
+			<span>File Name</span> \
+		</div>"
+	);
 
+	pathBarUpdate('/home/username');
 	fileSystemUpdate($fileSystemData);
+
+	// Initially set zIndex depths in order to use overlay with menu;
+	// also, show and hide relavant items.
 
 	$('div#overlay').css('zIndex', 0).hide();
 	$('div#file-system').css('z-index', 10).show();
@@ -59,11 +67,12 @@ $(document).ready(function() {
 	$('div.popup-menu').css('z-index', 20).hide();
 	
 
+
+	// Assign popup menu action handlers to both .icons and the background of #file-system:
 	$('div#file-system .icon').on('contextmenu', function(e) {
 		popupOpen($('div#icon-menu'), e.pageX, e.pageY);
 		return false;
 	});
-
 
 	$('div#file-system:not(.icon)').on('contextmenu', function(e) {
 		popupOpen($('div#space-menu'), e.pageX, e.pageY);
