db73c97fac4e623c61f8ea6568f5666e8e64010c
Kerrick Staley <mail@kerrickstaley.com>
2012-11-23 20:19:52 -0600

    Open WebSocket and send keystrokes
    The client now opens a websocket when the page is opened, and sends
    keystroke data when keys are pressed.

--- data/js/WsshTerminal.js
+++ data/js/WsshTerminal.js
@@ -9,5 +9,12 @@ function extend(subClass, baseClass) {
 function WsshTerminal(container)
 {
 	this.superClass.constructor.call(this, container);
+	this.sock = new WebSocket('ws://localhost:8001');
 }
 extend(WsshTerminal, VT100);
+
+WsshTerminal.prototype.keysPressed = function(keys)
+{
+	this.superClass.keysPressed.call(this, keys);
+	this.sock.send(JSON.stringify({ keys: keys }));
+}
