8dbc748b94ec7bd5271e573b9e964bdb95df64c2
Kerrick Staley <mail@kerrickstaley.com>
2012-12-02 23:12:54 -0600

    Make $shadeDiv cover everything
    Before, the #file-system div would poke through $shadeDiv (the semi-
    opaque div that appears on top of the UI when you're not connected).

--- data/js/base.js
+++ data/js/base.js
@@ -239,7 +239,8 @@ function doConnect() {
 		width: '100%',
 		height: '100%',
 		'background-color': '#000000',
-		opacity: 0.5
+		opacity: 0.5,
+		'z-index': 20
 	}).appendTo('body');
 	
 	var $authButton = $('<button type="button">Connect</button>').css({
