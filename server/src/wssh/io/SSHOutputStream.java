62098342f91b39af8fec82bf599bbc5ad0483964
Cyle Dawson <cyle@quadecagon.com>
2012-12-11 17:33:06 -0600

    Added some debug print statements to figure out what's wrong with my JSON.

--- server/src/wssh/io/SSHOutputStream.java
+++ server/src/wssh/io/SSHOutputStream.java
@@ -1,23 +1,32 @@
 package wssh.io;
 
 import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.java_websocket.WebSocket;
 
 public class SSHOutputStream extends OutputStream
 {
 	private WebSocket ws;
+	private Pattern escapeExpression;
 
 	public SSHOutputStream(WebSocket ws)
 	{
 		this.ws = ws;
+
+		this.escapeExpression = Pattern.compile("(\"|\\\\u)");
 	}
 
 	/** @Override */
 	public void write(int b)
 	{
+		//String character = this.escapeExpression.matcher("" + ((char) b)).replaceAll("\\\\$1");
 		String character = "" + ((char) b);
-		ws.send("{\"output\": \"" + character + "\"}");
+		System.out.println(character);
+		String token = "{\"output\": \"" + character + "\"}";
+		System.out.println(token);
+		ws.send(token);
 		//System.out.println(((char) b));
 	}
 }
